d587be2b083468fc0fe3abfb7763a2a04003bcbe
Pedro Marchetti Casteli <pedrocasteli68@gmail.com>
2025-12-03 14:33:33 -0300

    add routes for each CRUD operation

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const app = express();
 
 const PORT = 3000;
 
+app.get("/", (request, response) => {
+  response.send("This is a GET request at /");
+});
+
+app.post("/create", (request, response) => {
+  response.send("This is a POST request at /create");
+});
+
+app.put("/edit", (request, response) => {
+  response.send("This is a PUT request at /edit");
+});
+
+app.delete("/delete", (request, response) => {
+  response.send("This is a DELETE request at /delete");
+});
+
 app.listen(PORT, () => {
   console.log(`The server is running on port ${PORT}`);
   console.log(data);
