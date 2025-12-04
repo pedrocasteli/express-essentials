7121ea7be3ea725c5880db30e6f00b49c98aae7d
Pedro Marchetti Casteli <pedrocasteli68@gmail.com>
2025-12-04 15:07:02 -0300

    use middleware to serve static files

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 
 const PORT = 3000;
 
+app.use(express.static("./public"));
+
+app.use("/images", express.static("images"));
+
 app.get("/", (request, response) => {
   response.json(data);
 });
